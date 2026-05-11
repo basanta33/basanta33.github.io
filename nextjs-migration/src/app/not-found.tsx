6b1b86d92fcc6301a6d7825690bfc1fab8e881cf
Basanta Khakurel <basantakhakurel@gmail.com>
2026-05-11 14:18:12 +0200

    too much

--- nextjs-migration/src/app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import { SiteShell } from "@/components/site-shell";
-
-export default function NotFound() {
-  return (
-    <SiteShell title="Page not found">
-      <p>The requested page does not exist in the migrated Next.js app.</p>
-    </SiteShell>
-  );
-}
